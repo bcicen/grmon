612ec1cae3f01715697caf36f7d99c7047928a07
Bradley Cicenas <bradley@vektor.nyc>
2018-03-16 16:23:49 +0000

    separate agent from parser

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package grmon
 
 import (
 	"bytes"
-	"encoding/json"
-	"net/http"
 	"regexp"
-	"runtime/pprof"
 	"strconv"
 	"strings"
 )
@@ -94,25 +91,4 @@ func ReadThreads(buf bytes.Buffer) *ThreadCreate {
 	}
 
 	return t
-}
-
-func Start() { go http.ListenAndServe(":1234", nil) }
-
-func grmonHandler(w http.ResponseWriter, r *http.Request) {
-	var buf bytes.Buffer
-
-	pprof.Lookup("goroutine").WriteTo(&buf, 2)
-	routines := ReadRoutines(buf)
-
-	data, err := json.Marshal(routines)
-	if err != nil {
-		panic(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Write(data)
-}
-
-func init() {
-	http.HandleFunc("/debug/grmon", grmonHandler)
 }
